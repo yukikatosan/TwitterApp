8a9b6116163867c356a911a048ca2f9c630150ad
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-03-31 21:54:23 +0900

    初回セットアップ

--- TwitterApp/Controller/MainTabController.swift
+++ TwitterApp/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterApp
 //
 //  Created by 加藤雄輝 on 2023/03/31.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
 }
-
