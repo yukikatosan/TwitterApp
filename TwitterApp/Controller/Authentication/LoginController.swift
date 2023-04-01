f6c0b482ceb14789148b10fb383044098f88b9e3
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-01 20:20:12 +0900

    ログイン・登録画面の作成

---
+++ TwitterApp/Controller/Authentication/LoginController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginController.swift
+//  TwitterApp
+//
+//  Created by 加藤雄輝 on 2023/04/01.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "TwitterLogo")
+        return imageView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        
+    }
+    
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+    
+}
---
+++ TwitterApp/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  TwitterApp
+//
+//  Created by 加藤雄輝 on 2023/04/01.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+    
+}
