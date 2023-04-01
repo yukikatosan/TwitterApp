0c72f7023b303699b66529104b89049e889fa301
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-01 19:41:11 +0900

    actionButton(ツイートするボタン)の実装

--- TwitterApp/Controller/MainTabController.swift
+++ TwitterApp/Controller/MainTabController.swift
@@ -14,8 +14,9 @@ class MainTabController: UITabBarController {
     let actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-        button.backgroundColor = .blue
+        button.backgroundColor = .twitterBlue
         button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -27,6 +28,13 @@ class MainTabController: UITabBarController {
         configureViewControllers()
         configureUI()
     }
+    
+    // MARK: - Selectors
+    
+    @objc func actionButtonTapped() {
+        print(123)
+    }
+    
     
     // MARK: - Helpers
     
@@ -56,11 +64,12 @@ class MainTabController: UITabBarController {
     
     func configureUI() {
         view.addSubview(actionButton)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-        actionButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.widthAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64).isActive = true
-        actionButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                            right: view.rightAnchor,
+                            paddingBottom: 64,
+                            paddingRight: 16,
+                            width: 56,
+                            height: 56)
         actionButton.layer.cornerRadius = 56 / 2
     }
 
