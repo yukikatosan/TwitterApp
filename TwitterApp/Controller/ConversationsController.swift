9ab48449b8f9410655bd90f3f381805a640f1c65
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-01 17:13:40 +0900

    NavigationBarに画像・タイトルを設定

--- TwitterApp/Controller/ConversationsController.swift
+++ TwitterApp/Controller/ConversationsController.swift
@@ -15,9 +15,13 @@ class ConversationsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPurple
+        configureUI()
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Messages"
+    }
 }
--- TwitterApp/Controller/NotificationsController.swift
+++ TwitterApp/Controller/NotificationsController.swift
@@ -15,9 +15,13 @@ class NotificationsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        configureUI()
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+    }
 }
