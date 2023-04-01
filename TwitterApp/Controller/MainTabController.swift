3b9174d9248b4c52a2ab89b247f19cd05d8b6d22
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-01 17:09:00 +0900

    UINavigationControllerを設定

--- TwitterApp/Controller/MainTabController.swift
+++ TwitterApp/Controller/MainTabController.swift
@@ -23,11 +23,26 @@ class MainTabController: UITabBarController {
     
     func configureViewControllers() {
         let feed = FeedController()
-        let explore = ExploreController()
-        let notifications = NotificationsController()
-        let conversations = ConversationsController()
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         
-        viewControllers = [feed, explore, notifications, conversations]
+        let explore = ExploreController()
+        let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
+        
+        let notifications = NotificationsController()
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
+        
+        let conversations = ConversationsController()
+        let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversations)
+        
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = image
+        nav.navigationBar.barTintColor = .white
+        
+        return nav
     }
 
 }
