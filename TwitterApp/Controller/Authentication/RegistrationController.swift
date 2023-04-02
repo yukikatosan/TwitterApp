5c7cbbcfc1104759106bbd077a9d7c2068b666a3
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-02 22:25:03 +0900

    プロフィール画像設定用のimagePickerを実装

--- TwitterApp/Controller/Authentication/RegistrationController.swift
+++ TwitterApp/Controller/Authentication/RegistrationController.swift
@@ -11,6 +11,8 @@ class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
+    private let imagePicker = UIImagePickerController()
+    
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "plus_photo"), for: .normal)
@@ -94,7 +96,7 @@ class RegistrationController: UIViewController {
     // MARK: - Selectors
     
     @objc func handleAddProfilePhoto() {
-        print("Add photo...")
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @objc func handRegistration() {
@@ -110,9 +112,12 @@ class RegistrationController: UIViewController {
     func configureUI() {
         view.backgroundColor = .twitterBlue
         
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
         view.addSubview(plusPhotoButton)
         plusPhotoButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
-        plusPhotoButton.setDimensions(width: 150, height: 150)
+        plusPhotoButton.setDimensions(width: 128, height: 128)
         
         let stack = UIStackView(arrangedSubviews: [emailContainerView,
                                                    passwordContainerView,
@@ -134,5 +139,22 @@ class RegistrationController: UIViewController {
                                      right: view.rightAnchor,
                                      paddingLeft: 40,
                                      paddingRight: 40)
+    }
+}
+
+extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let profileImage = info[.editedImage] as? UIImage else { return }
+        plusPhotoButton.layer.cornerRadius = 128 / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.imageView?.contentMode = .scaleAspectFill
+        plusPhotoButton.imageView?.clipsToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        self.plusPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        dismiss(animated: true, completion: nil)
     }
 }
