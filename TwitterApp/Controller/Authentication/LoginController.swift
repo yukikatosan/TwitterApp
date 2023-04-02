11d8007756a33b74c72a39cdcf69b15a7e4a161e
Yuki Kato <yuuki.25.02.1994@gmail.com>
2023-04-02 16:25:38 +0900

    ログインボタンの実装

--- TwitterApp/Controller/Authentication/LoginController.swift
+++ TwitterApp/Controller/Authentication/LoginController.swift
@@ -42,6 +42,17 @@ class LoginController: UIViewController {
         return textField
     }()
     
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -51,6 +62,10 @@ class LoginController: UIViewController {
     
     
     // MARK: - Selectors
+    
+    @objc func handleLogin() {
+        print("Handle login here...")
+    }
     
     // MARK: - Helpers
     
@@ -63,9 +78,12 @@ class LoginController: UIViewController {
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
         stack.axis = .vertical
         stack.spacing = 20
+        stack.distribution = .fillEqually
         
         view.addSubview(stack)
         stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
